1e9711bfd82caf0b8634c7e21fdbf0c9322a147f
Matthew Sigley <matthew.d.sigley@gmail.com>
2015-06-05 11:21:08 -0400

    Added support for images without placeholder.

--- jquery.unveil.js
+++ jquery.unveil.js
@@ -10,47 +10,89 @@
 
 ;(function($) {
 
-  $.fn.unveil = function(threshold, callback) {
+$.fn.unveil = function(threshold, callback) {
+	
+	var $w = $(window),
+		th = threshold || 0,
+		retina = window.devicePixelRatio > 1,
+		attrib = retina? "data-src-retina" : "data-src",
+		images = this,
+		loaded;
+		
+	if( document.readyState == 'complete' ) //Do nothing for pages loaded from cache
+		return this;
+	
+	function isImageLoaded(img) {
+	    if (!img.complete)
+	        return false;
+	    if (typeof img.naturalWidth != "undefined" && img.naturalWidth == 0)
+	        return false;
+	    return true;
+	}
+	
+	images.each(function() { //Lazy load images without placeholders
+		var thisElement = $(this),
+			thisHidden = thisElement.is(":hidden"),
+			dataSource = this.getAttribute(attrib);
+			dataSource = dataSource || this.getAttribute("data-src");
+		
+		if (dataSource || thisHidden || isImageLoaded(this)) return;
+		
+		var source = this.getAttribute("src");
+		this.setAttribute("data-src", source);
+		this.setAttribute("src", "");
+		if( !thisElement.width() ) {
+			thisElement.width(1);
+			this.setAttribute("data-fake-width", true);
+		}
+		if( !thisElement.height() ) {
+			thisElement.height(1);
+			this.setAttribute("data-fake-height", true);
+		}
+		if( 'inline' == thisElement.css('display') )
+			thisElement.css('display', 'inline-block');
+	});
+	
+	images.one("unveil", function() {
+		var thisElement = $(this),
+			source = this.getAttribute(attrib);
+			source = source || this.getAttribute("data-src");
+		if (source) {
+			this.setAttribute("src", source);
+			if (typeof callback === "function") callback.call(this);
+		}
+		if (this.getAttribute("data-fake-width")) {
+			thisElement.width('auto');
+			this.setAttribute("data-fake-width", false);
+		}
+		if (this.getAttribute("data-fake-height")) {
+			thisElement.height('auto');
+			this.setAttribute("data-fake-height", false);
+		}
+	});
+	
+	function unveil() {
+		var inview = images.filter(function() {
+			var $e = $(this);
+			if ($e.is(":hidden")) return;
+			
+			var wt = $w.scrollTop(),
+			wb = wt + $w.height(),
+			et = $e.offset().top,
+			eb = et + $e.height();
+			
+			return eb >= wt - th && et <= wb + th;
+		});
+		loaded = inview.trigger("unveil");
+		images = images.not(loaded);
+	}
+	
+	$w.on("scroll.unveil resize.unveil lookup.unveil", unveil);
+	
+	unveil();
+	
+	return this;
 
-    var $w = $(window),
-        th = threshold || 0,
-        retina = window.devicePixelRatio > 1,
-        attrib = retina? "data-src-retina" : "data-src",
-        images = this,
-        loaded;
-
-    this.one("unveil", function() {
-      var source = this.getAttribute(attrib);
-      source = source || this.getAttribute("data-src");
-      if (source) {
-        this.setAttribute("src", source);
-        if (typeof callback === "function") callback.call(this);
-      }
-    });
-
-    function unveil() {
-      var inview = images.filter(function() {
-        var $e = $(this);
-        if ($e.is(":hidden")) return;
-
-        var wt = $w.scrollTop(),
-            wb = wt + $w.height(),
-            et = $e.offset().top,
-            eb = et + $e.height();
-
-        return eb >= wt - th && et <= wb + th;
-      });
-
-      loaded = inview.trigger("unveil");
-      images = images.not(loaded);
-    }
-
-    $w.on("scroll.unveil resize.unveil lookup.unveil", unveil);
-
-    unveil();
-
-    return this;
-
-  };
+};
 
 })(window.jQuery || window.Zepto);
